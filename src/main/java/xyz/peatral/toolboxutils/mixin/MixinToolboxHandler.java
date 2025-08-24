b478e1c7485add2d29149755fcfd1fc3290b9128
Christof Reimers <contact@peatral.xyz>
2025-08-24 13:22:47 +0200

    Fix less toolboxes being returned than possible

--- src/main/java/xyz/peatral/toolboxutils/mixin/MixinToolboxHandler.java
+++ src/main/java/xyz/peatral/toolboxutils/mixin/MixinToolboxHandler.java
@@ -4,27 +4,40 @@ import com.simibubi.create.content.equipment.toolbox.ToolboxBlockEntity;
 import com.simibubi.create.content.equipment.toolbox.ToolboxHandler;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.peatral.toolboxutils.IEnchantableToolbox;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Mixin(ToolboxHandler.class)
 public class MixinToolboxHandler {
-    @Inject(method = "getNearest", at = @At("RETURN"), remap = false, cancellable = true)
+    @Inject(method = "getNearest", at = @At("HEAD"), remap = false, cancellable = true)
     private static void getNearest(LevelAccessor world, Player player, int maxAmount, CallbackInfoReturnable<List<ToolboxBlockEntity>> cir) {
-        cir.setReturnValue(cir.getReturnValue().stream().filter(toolboxBlockEntity -> {
-            if (toolboxBlockEntity instanceof IEnchantableToolbox tb) {
-                return tb.create_toolbox_utils$getLoyaltyLevel() < 1
-                        || tb.create_toolbox_utils$isOwner(player);
-            } else {
-                return true;
-            }
-        }).collect(Collectors.toList()));
+        Vec3 location = player.position();
+        double maxRange = ToolboxHandler.getMaxRange(player);
+        List<ToolboxBlockEntity> returnValue = ToolboxHandler.toolboxes.get(world)
+                .keySet()
+                .stream()
+                .filter(p -> ToolboxHandler.distance(location, p) < maxRange * maxRange)
+                .sorted(Comparator.comparingDouble(p -> ToolboxHandler.distance(location, p)))
+                .limit(maxAmount)
+                .map(ToolboxHandler.toolboxes.get(world)::get)
+                .filter(ToolboxBlockEntity::isFullyInitialized)
+                .filter(toolboxBlockEntity -> {
+                    if (toolboxBlockEntity instanceof IEnchantableToolbox tb) {
+                        return tb.create_toolbox_utils$getLoyaltyLevel() < 1
+                                || tb.create_toolbox_utils$isOwner(player);
+                    }
+                    return true;
+                })
+                .collect(Collectors.toList());
+        cir.setReturnValue(returnValue);
     }
 
     @Inject(method = "withinRange", at = @At("RETURN"), remap = false, cancellable = true)
