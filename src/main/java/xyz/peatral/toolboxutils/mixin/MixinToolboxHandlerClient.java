9f59a606ea7e6ba0cbd83a614c567018cf1a8e99
Christof Reimers <contact@peatral.xyz>
2026-04-30 13:38:57 +0200

    Render hotbar overlay for proxies

--- src/main/java/xyz/peatral/toolboxutils/mixin/MixinToolboxHandlerClient.java
+++ src/main/java/xyz/peatral/toolboxutils/mixin/MixinToolboxHandlerClient.java
@@ -1,4 +1,78 @@
 package xyz.peatral.toolboxutils.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.simibubi.create.content.equipment.toolbox.ToolboxHandler;
+import com.simibubi.create.content.equipment.toolbox.ToolboxHandlerClient;
+import com.simibubi.create.foundation.gui.AllGuiTextures;
+import net.createmod.catnip.nbt.NBTHelper;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtUtils;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.peatral.toolboxutils.proxy.ToolboxProxy;
+import xyz.peatral.toolboxutils.proxy.ToolboxProxyHandler;
+
+import java.util.UUID;
+
+import static com.simibubi.create.foundation.gui.AllGuiTextures.*;
+import static com.simibubi.create.foundation.gui.AllGuiTextures.TOOLBELT_HOTBAR_OFF;
+
+@Mixin(ToolboxHandlerClient.class)
 public class MixinToolboxHandlerClient {
+    @Inject(
+            method = "renderOverlay",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/player/Player;getPersistentData()Lnet/minecraft/nbt/CompoundTag;",
+                    ordinal = 0
+            )
+    )
+    private static void renderOverlay(
+            GuiGraphics guiGraphics,
+            DeltaTracker deltaTracker,
+            CallbackInfo ci,
+            @Local(name = "x") int x,
+            @Local(name = "y") int y,
+            @Local(name = "player") Player player
+    ) {
+        CompoundTag persistentData = player.getPersistentData();
+        if (!persistentData.contains("CreateToolboxProxyData"))
+            return;
+
+        CompoundTag compound = player.getPersistentData()
+                .getCompound("CreateToolboxProxyData");
+
+        if (compound.isEmpty())
+            return;
+
+        PoseStack poseStack = guiGraphics.pose();
+        poseStack.pushPose();
+        for (int slot = 0; slot < 9; slot++) {
+            String key = String.valueOf(slot);
+            if (!compound.contains(key))
+                continue;
+            UUID uuid = NbtUtils.loadUUID(NBTHelper.getINBT(compound.getCompound(key), "UUID"));
+            ToolboxProxy proxy = ToolboxProxyHandler.getProxy(player.level(), uuid);
+            if (proxy == null) {
+                continue;
+            }
+            BlockPos pos = proxy.getBlockPos();
+            double max = ToolboxHandler.getMaxRange(player);
+            boolean selected = player.getInventory().selected == slot;
+            int offset = selected ? 1 : 0;
+            AllGuiTextures texture = ToolboxHandler.distance(player.position(), pos) < max * max
+                    ? selected ? TOOLBELT_SELECTED_ON : TOOLBELT_HOTBAR_ON
+                    : selected ? TOOLBELT_SELECTED_OFF : TOOLBELT_HOTBAR_OFF;
+            texture.render(guiGraphics, x + 20 * slot - offset, y + offset);
+        }
+        poseStack.popPose();
+    }
+
 }
