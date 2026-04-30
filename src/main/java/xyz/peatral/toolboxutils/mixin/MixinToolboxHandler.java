d90d6a5122dd85c2cc2f4f3fcce69b12eead8d83
Christof Reimers <contact@peatral.xyz>
2026-04-30 14:51:52 +0200

    Cleanup MixinToolboxHandler code

--- src/main/java/xyz/peatral/toolboxutils/mixin/MixinToolboxHandler.java
+++ src/main/java/xyz/peatral/toolboxutils/mixin/MixinToolboxHandler.java
@@ -1,126 +1,135 @@
 package xyz.peatral.toolboxutils.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import com.simibubi.create.content.equipment.toolbox.ToolboxBlockEntity;
 import com.simibubi.create.content.equipment.toolbox.ToolboxHandler;
+import net.createmod.catnip.nbt.NBTHelper;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
 import net.minecraft.nbt.Tag;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.peatral.toolboxutils.IEnchantableToolbox;
 import xyz.peatral.toolboxutils.proxy.ToolboxProxy;
 import xyz.peatral.toolboxutils.proxy.ToolboxProxyHandler;
 
-import java.util.Comparator;
-import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 import static com.simibubi.create.content.equipment.toolbox.ToolboxHandler.syncData;
 
 @Mixin(ToolboxHandler.class)
 public class MixinToolboxHandler {
 
-    @Unique
-    private static final int create_toolbox_utils$validationTimer = 20;
-
-    @Inject(method = "getNearest", at = @At("HEAD"), remap = false, cancellable = true)
-    private static void getNearest(LevelAccessor world, Player player, int maxAmount, CallbackInfoReturnable<List<ToolboxBlockEntity>> cir) {
-        Vec3 location = player.position();
-        double maxRange = ToolboxHandler.getMaxRange(player);
-        List<ToolboxBlockEntity> returnValue = ToolboxHandler.toolboxes.get(world)
-                .keySet()
-                .stream()
-                .filter(p -> ToolboxHandler.distance(location, p) < maxRange * maxRange)
-                .sorted(Comparator.comparingDouble(p -> ToolboxHandler.distance(location, p)))
-                .limit(maxAmount)
-                .map(ToolboxHandler.toolboxes.get(world)::get)
-                .filter(ToolboxBlockEntity::isFullyInitialized)
-                .filter(toolboxBlockEntity -> {
-                    if (toolboxBlockEntity instanceof IEnchantableToolbox tb) {
-                        return tb.create_toolbox_utils$getLoyaltyLevel(world.registryAccess()) < 1
-                                || tb.create_toolbox_utils$isOwner(player);
-                    }
-                    return true;
-                })
-                .collect(Collectors.toList());
-        cir.setReturnValue(returnValue);
-    }
-
-    @Inject(method = "withinRange", at = @At("RETURN"), remap = false, cancellable = true)
-    private static void withinRange(Player player, ToolboxBlockEntity box, CallbackInfoReturnable<Boolean> cir) {
-        if (!(box instanceof IEnchantableToolbox tb) || tb.create_toolbox_utils$getLoyaltyLevel(player.level().registryAccess()) < 3 || !tb.create_toolbox_utils$isOwner(player)) {
-            return;
-        }
-        cir.setReturnValue(true);
-    }
-
-    @Inject(method = "entityTick", at = @At(value = "HEAD"))
-    private static void entityTick(Entity entity, Level world, CallbackInfo ci) {
-        if (world.isClientSide)
-            return;
-        if (!(world instanceof ServerLevel))
-            return;
-        if (!(entity instanceof ServerPlayer player))
-            return;
-        if (entity.tickCount % create_toolbox_utils$validationTimer != 0)
-            return;
-
-        if (!player.getPersistentData()
-                .contains("CreateToolboxProxyData"))
-            return;
-
-        boolean sendData = false;
-        CompoundTag compound = player.getPersistentData()
-                .getCompound("CreateToolboxProxyData");
-        for (int i = 0; i < 9; i++) {
-            String key = String.valueOf(i);
-            if (!compound.contains(key))
-                continue;
-
-            CompoundTag data = compound.getCompound(key);
-            Tag uuidTag = data.get("UUID");
-            UUID uuid = NbtUtils.loadUUID(uuidTag);
-            int slot = data.getInt("Slot");
-
-            // TODO: Not sure how to handle unloaded entities
-            if (!ToolboxProxyHandler.hasProxy(world, uuid)) {
-                compound.remove(key);
-                sendData = true;
-                continue;
+    @WrapOperation(
+            method = "getNearest",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/util/stream/Stream;filter(Ljava/util/function/Predicate;)Ljava/util/stream/Stream;",
+                    ordinal = 1
+            ),
+            remap = false
+    )
+    private static Stream<ToolboxBlockEntity> getNearest(
+            Stream<ToolboxBlockEntity> instance,
+            Predicate<ToolboxBlockEntity> predicate,
+            Operation<Stream<ToolboxBlockEntity>> original,
+            @Local(argsOnly = true) LevelAccessor world,
+            @Local(argsOnly = true) Player player
+    ) {
+        return original.call(instance, predicate.and(toolboxBlockEntity -> {
+            if (toolboxBlockEntity instanceof IEnchantableToolbox tb) {
+                return tb.create_toolbox_utils$getLoyaltyLevel(world.registryAccess()) < 1
+                        || tb.create_toolbox_utils$isOwner(player);
             }
-
-            ToolboxProxyHandler.getProxy(world, uuid).connectPlayer(slot, player, i);
-        }
-
-        // TODO: Duplicate sync?????
-        if (sendData)
-            syncData(player);
+            return true;
+        }));
     }
 
-    @Inject(method = "playerLogin", at = @At(value = "HEAD"))
-    private static void playerLogin(Player player, CallbackInfo ci) {
-        if (!(player instanceof ServerPlayer))
-            return;
-        if (player.getPersistentData()
-                .contains("CreateToolboxProxyData")
-                && !player.getPersistentData()
-                .getCompound("CreateToolboxProxyData")
-                .isEmpty()) {
-            // TODO: Duplicate sync?????
+    @WrapMethod(method = "withinRange", remap = false)
+    private static boolean withinRange(Player player, ToolboxBlockEntity box, Operation<Boolean> original) {
+        boolean isOwnedByPlayer = box instanceof IEnchantableToolbox tb
+                && tb.create_toolbox_utils$getLoyaltyLevel(player.level().registryAccess()) >= 3
+                && tb.create_toolbox_utils$isOwner(player);
+        return isOwnedByPlayer || original.call(player, box);
+    }
+
+    @WrapOperation(
+            method = "entityTick",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/nbt/CompoundTag;contains(Ljava/lang/String;)Z",
+                    ordinal = 0
+            )
+    )
+    private static boolean wrapEntityTickValidation(
+            CompoundTag persistentData,
+            String persistentDataKey,
+            Operation<Boolean> original,
+            @Local(name = "player") ServerPlayer player,
+            @Local(argsOnly = true) Level world
+    ) {
+        boolean proxyChangedData = false;
+        if (persistentData.contains("CreateToolboxProxyData")) {
+            CompoundTag compound = persistentData.getCompound("CreateToolboxProxyData");
+            for (int i = 0; i < 9; i++) {
+                String key = String.valueOf(i);
+                if (!compound.contains(key))
+                    continue;
+
+                CompoundTag data = compound.getCompound(key);
+                UUID uuid = NbtUtils.loadUUID(NBTHelper.getINBT(data, "UUID"));
+                int slot = data.getInt("Slot");
+
+                // TODO: Not sure how to handle unloaded entities
+                if (!ToolboxProxyHandler.hasProxy(world, uuid)) {
+                    compound.remove(key);
+                    proxyChangedData = true;
+                    continue;
+                }
+
+                ToolboxProxy proxy = ToolboxProxyHandler.getProxy(world, uuid);
+                if (proxy != null) {
+                    proxy.connectPlayer(slot, player, i);
+                }
+            }
+        }
+
+        boolean originalHasData = original.call(persistentData, persistentDataKey);
+
+        if (proxyChangedData && !originalHasData) {
             syncData(player);
         }
+
+        return originalHasData;
+    }
+
+    @ModifyExpressionValue(
+            method = "playerLogin",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/nbt/CompoundTag;isEmpty()Z"
+            )
+    )
+    private static boolean wrapPlayerLoginSyncCheck(boolean originalCondition, Player player) {
+        if (originalCondition) {
+            return true;
+        }
+
+        CompoundTag persistentData = player.getPersistentData();
+        return persistentData.contains("CreateToolboxProxyData")
+                && !persistentData.getCompound("CreateToolboxProxyData").isEmpty();
     }
 
     @Inject(method = "unequip", at = @At(value = "HEAD"))
@@ -133,8 +142,7 @@ public class MixinToolboxHandler {
             return;
 
         CompoundTag prevData = compound.getCompound(key);
-        Tag uuidTag = prevData.get("UUID");
-        UUID prevUUID = NbtUtils.loadUUID(uuidTag);
+        UUID prevUUID = NbtUtils.loadUUID(NBTHelper.getINBT(prevData, "UUID"));
         int prevSlot = prevData.getInt("Slot");
 
         ToolboxProxy prevProxy = ToolboxProxyHandler.getProxy(world, prevUUID);
