4c6b9f21934099fa18e94bbdb989bf27e4169acc
Christof Reimers <contact@peatral.xyz>
2026-04-30 02:23:14 +0200

    Enable toolbox proxy for allay

---
+++ src/main/java/xyz/peatral/toolboxutils/mixin/MixinAllay.java
@@ -0,0 +1,41 @@
+package xyz.peatral.toolboxutils.mixin;
+
+import com.simibubi.create.AllTags;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.animal.allay.Allay;
+import net.minecraft.world.entity.npc.InventoryCarrier;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.gameevent.vibrations.VibrationSystem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.peatral.toolboxutils.ToolboxProxyHandler;
+
+@Mixin(Allay.class)
+public abstract class MixinAllay extends PathfinderMob implements InventoryCarrier, VibrationSystem {
+    protected MixinAllay(EntityType<? extends PathfinderMob> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Inject(method = "tick", at = @At("RETURN"))
+    private void tick(CallbackInfo ci) {
+        ItemStack stack = getItemInHand(InteractionHand.MAIN_HAND);
+        if (AllTags.AllItemTags.TOOLBOXES.matches(stack)) {
+            ToolboxProxyHandler.tickToolbox(this, stack);
+        }
+    }
+
+    @Override
+    public void setItemSlot(EquipmentSlot slot, ItemStack stack) {
+        ItemStack handStack = getItemBySlot(EquipmentSlot.MAINHAND);
+        if (slot == EquipmentSlot.MAINHAND && AllTags.AllItemTags.TOOLBOXES.matches(handStack)) {
+            ToolboxProxyHandler.removeToolbox(this, handStack);
+        }
+        super.setItemSlot(slot, stack);
+    }
+}
